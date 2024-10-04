90115d4531a6ff56de51cefc09135b514eeb5b4e
Aldair <acondoriat@unsa.edu.pe>
2024-10-03 23:10:29 -0500

    Intento de implementacion de la funcion flipV()

--- ejemplo.c
+++ ejemplo.c
@@ -1,7 +1,19 @@
 #include "chess.h"
 #include "figures.h"
+#include <stdio.h>
 
-void display(){
-  char** blackKnight = reverse(knight);
-  interpreter(blackKnight);
+void flipV1(char** var1){
+	
+    for(int i = 0; *(*var1 + i) != 0; i++)
+	for(int j = 0; var1[i][j] != '\0'; j++){
+		printf("%s", var1[i][j]);
+	}
+
+}
+
+int main(){
+
+    flipV1(whiteSquare);	
+
+    return 0;
 }
