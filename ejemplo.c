82635fe866d0f0cd8116bb60f8fbf6db405a6c6f
FERNANDO <fllosam@unsa.edu.pe>
2024-10-04 22:46:16 -0500

    casillas negras han llegadogit add .git add .git add .

--- ejemplo.c
+++ ejemplo.c
@@ -1,8 +1,10 @@
 #include "chess.h"
 #include "figures.h"
 #include <stdio.h>
-void display(){
-  char** result= repeatH(whiteSquare,4);
+void display()
+{
+  char** blackSquare= reverse(whiteSquare);
+  char** result= repeatH(blackSquare,4);
   interpreter(result);
 }
 
