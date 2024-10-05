ef83e8f9868a963ea5af5d7dcb57bba7e73ecdbb
jhosep <jcacsire@unsa.edu.pe>
2024-10-04 22:28:27 -0500

    Probando repeatH con la figura whitesquare

--- ejemplo.c
+++ ejemplo.c
@@ -1,19 +1,8 @@
 #include "chess.h"
 #include "figures.h"
 #include <stdio.h>
-
-void flipV1(char** var1){
-	
-    for(int i = 0; *(*var1 + i) != 0; i++)
-	for(int j = 0; var1[i][j] != '\0'; j++){
-		printf("%s", var1[i][j]);
-	}
-
+void display(){
+  char** result= repeatH(whiteSquare,4);
+  interpreter(result);
 }
 
-int main(){
-
-    flipV1(whiteSquare);	
-
-    return 0;
-}
