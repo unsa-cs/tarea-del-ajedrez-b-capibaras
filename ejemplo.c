34e46ab730e8461ae9f97088ff77006ea3dde6fe
FERNANDO <fllosam@unsa.edu.pe>
2024-10-06 17:24:01 -0500

    empezando a utilizar bucles tacticos para crear

--- ejemplo.c
+++ ejemplo.c
@@ -13,7 +13,6 @@ void display()
   char** tablero=repeatV(dosLineasUnidas,2);
   */
   char** blackSquare=reverse(whiteSquare);
-
   //
   char** c_torre=superImpose(rook,blackSquare);
   char** c_caballo=superImpose(knight,whiteSquare);
@@ -24,6 +23,15 @@ void display()
   char** c_caballo2 = superImpose(knight,blackSquare);
   char** c_torre2=superImpose(rook,whiteSquare);
   //
+  char** fic_cas[8]={c_torre,c_caballo,c_alfil,c_dama,c_rey,c_alfil2,c_caballo2,c_torre2};
+  char** uniones[4];
+  int p=0;
+  while(p<4)
+  {
+    uniones[p]=join(*(fic_cas),*(fic_cas+1));
+    *fic_cas+=2;
+    p++;
+  }
   char** unirAlfilDama = join(c_alfil, c_dama);
   char** unidos=join(c_torre,c_caballo);
   char** unirReyAlfil=join(c_rey,c_alfil2);
@@ -31,15 +39,15 @@ void display()
   char** unidos2 = join(unidos, unirAlfilDama);
   char** unidos3=join(unirReyAlfil,unirCaballo2Torre2);
   char** unionFinal=join(unidos2, unidos3);
-
+  
   char** result1 = superImpose(pawn, whiteSquare);
   char** result2 = superImpose(pawn, blackSquare);
-  char** result = repeatH(join(result1, result2), 4);
+  char** filapeones = repeatH(join(result1, result2), 4);
 
-  char** equipoBlanco = up(unionFinal, result);
+  char** equipoBlanco = up(unionFinal, filapeones);
   char** lineasunidas = up(primeraLinea,segundaLinea);
   char** resultado = up(equipoBlanco,lineasunidas);
-  interpreter(resultado);
+  interpreter(uniones[0]);
   
   int i=0;
   while (unionFinal[i]!=0)
